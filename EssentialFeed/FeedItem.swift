267564f50be3cb802e16e76d7ea01452c096c155
Joy <joy.1207@kakaovx.com>
2023-03-13 19:10:17 +0900

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Joy on 2023/03/13.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
