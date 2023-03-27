7b3663f06242e86efebdcf7e6d74f30107f4da99
Joy <joy.1207@kakaovx.com>
2023-03-27 17:15:30 +0900

    Replace optional params with enum with success/failure cases

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -5,9 +5,14 @@
 //  Created by Joy on 2023/03/22.
 //
 
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
+
 // 느슨한 결합을 위해 인터페이스(= 프로토콜)로 변경 + 동작의 구현이 없어지는 것이 중요한 포인트
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -25,10 +30,11 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { result in
+            switch result {
+            case .success:
                 completion(.invalidData)
-            } else {
+            case .failure:
                 completion(.connectivity)
             }
         }
