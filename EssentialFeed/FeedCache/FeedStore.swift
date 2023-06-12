7be547956aa3533bbe4b2cecdf60fdfe3a85c369
Joy <joy.1207@kakaovx.com>
2023-06-12 17:34:47 +0900

    Move 'FeedStore' to its own file

---
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Joy on 2023/06/12.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompeltion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompeltion)
+}
