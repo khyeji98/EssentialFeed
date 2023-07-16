a87bad2d6bf48767a47e962deafb31035e4ba1b1
Joy <joy.1207@kakaovx.com>
2023-07-17 08:54:56 +0900

    Move 'FeedCachePolicy' to its own file

---
+++ EssentialFeed/FeedCache/FeedCachePolicy.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Joy on 2023/07/17.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
