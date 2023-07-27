019c2bddb90d88a86ec66e5adefecb6986ad6d2b
Joy <joy.1207@kakaovx.com>
2023-07-27 09:14:41 +0900

    Retrieving from empty cache delivers empty result

--- EssentialFeed/FeedCache/FeedCachePolicy.swift
+++ EssentialFeed/FeedCache/FeedCachePolicy.swift
@@ -13,7 +13,7 @@ internal final class FeedCachePolicy {
     private static let calendar = Calendar(identifier: .gregorian)
     
     private static var maxCacheAgeInDays: Int {
-        7
+        3
     }
     
     internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
